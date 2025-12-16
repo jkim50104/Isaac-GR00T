b9df488f19a49f9579ce53663fda14c63f7bb74d
jkim50104 <jkim50104@gmail.com>
2025-12-16 18:48:18 +0900

    install script update for ubuntu 20.04

--- install_scripts/cu124_1.6.sh
+++ install_scripts/cu124_1.6.sh
@@ -15,4 +15,8 @@ pip install uv
 uv sync --python 3.10
 uv pip install -e .
 
-# pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+# For ubuntu 20.04 /lib/x86_64-linux-gnu/libc.so.6: version `GLIBC_2.32' not found. Have to be >= 22.04
+# So downgrade torch and recompile flash attention with the downgraded torch
+uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+uv pip uninstall flash-attn
+pip install --no-build-isolation flash-attn==2.7.4.post1
