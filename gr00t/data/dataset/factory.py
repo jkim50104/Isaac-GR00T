56051dd6a5220c2cdda59320890e87326d9d1d66
jkim50104 <jkim50104@gmail.com>
2026-01-06 23:10:06 +0900

    fintune script edit

--- gr00t/data/dataset/factory.py
+++ gr00t/data/dataset/factory.py
@@ -45,7 +45,7 @@ class DatasetFactory:
                 else:
                     generate_stats(dataset_path)
                     generate_rel_stats(dataset_path, EmbodimentTag(embodiment_tag))
-                torch.distributed.barrier()
+                # torch.distributed.barrier()
                 dataset = ShardedSingleStepDataset(
                     dataset_path=dataset_path,
                     embodiment_tag=EmbodimentTag(embodiment_tag),
--- help_scripts/finetune_process/fintune_new_embodiment.sh
+++ help_scripts/finetune_process/fintune_new_embodiment.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
+BATCH_SIZE=160
+ARM_ONLY=true
+
+# Default values
+HYPER_PARAMS="B${BATCH_SIZE}"
+CONFIG="ai_worker_config.py"
+
+# Apply ARM_ONLY overrides
+if [ "$ARM_ONLY" = true ]; then
+  HYPER_PARAMS="B${BATCH_SIZE}_AO"
+  CONFIG="ai_worker_config_arm_only.py"
+fi
+
 # Run finetuning process with the new embodiment tag
 # Configure for single GPU
-export NUM_GPUS=2
-export CUDA_VISIBLE_DEVICES="2,3"
+export NUM_GPUS=1
+export CUDA_VISIBLE_DEVICES=$1
+
 python gr00t/experiment/launch_finetune.py \
     --base-model-path nvidia/GR00T-N1.6-3B \
     --dataset-path ./data/ai_worker/ffw_sg2_rev1_clear_item \
     --embodiment-tag NEW_EMBODIMENT \
-    --modality-config-path ./data/ai_worker/ai_worker_config.py \
+    --modality-config-path ./data/ai_worker/$CONFIG \
     --num-gpus $NUM_GPUS \
-    --output-dir ./output/ai_worker_160BT_2GPU \
+    --output-dir ./output/ai_worker_${HYPER_PARAMS}_REL \
     --save-total-limit 5 \
-    --save-steps 2000 \
-    --max-steps 10000 \
+    --save-steps 5000 \
+    --max-steps 20000 \
     --use-wandb \
-    --global-batch-size 160 \
+    --global-batch-size $BATCH_SIZE \
     --color-jitter-params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
     --dataloader-num-workers 4
