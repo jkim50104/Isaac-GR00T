ab4f581685d15d299aba1f4def8342014fbae670
jkim50104 <jkim50104@gmail.com>
2026-01-13 16:15:01 +0900

    changed config dir, finetuning drive

---
+++ help_scripts/data_config/ai_worker_arm_only_config.py
@@ -0,0 +1,63 @@
+from gr00t.configs.data.embodiment_configs import register_modality_config
+from gr00t.data.embodiment_tags import EmbodimentTag
+from gr00t.data.types import ModalityConfig, ActionConfig, ActionRepresentation, ActionType, ActionFormat
+
+ai_worker = {
+    "video": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["ego_view",
+                       "left_wrist_view",
+                       "right_wrist_view",
+                       ],
+    ),
+    "state": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=[
+            "left_arm",
+            "left_gripper",
+            "right_arm",
+            "right_gripper",
+        ],
+    ),
+    "action": ModalityConfig(
+        delta_indices=list(range(0, 32)),
+        modality_keys=[
+            "left_arm",
+            "left_gripper",
+            "right_arm",
+            "right_gripper",
+        ],
+        action_configs=[
+            # left_arm
+            ActionConfig(
+                rep=ActionRepresentation.RELATIVE, # RELATIVE, ABSOLUTE
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+            # left_hand
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,  # G1 hand is controlled by binary signals like a gripper
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+            # right_arm
+            ActionConfig(
+                rep=ActionRepresentation.RELATIVE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+            # right_hand
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,  # G1 hand is controlled by binary signals like a gripper
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+        ],
+    ),
+    "language": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["annotation.human.action.task_description"],
+    ),
+}
+
+register_modality_config(ai_worker, embodiment_tag=EmbodimentTag.NEW_EMBODIMENT)
--- help_scripts/finetune_process/fintune_new_embodiment.sh
+++ help_scripts/finetune_process/fintune_new_embodiment.sh
@@ -14,8 +14,8 @@ case "${SERVER}" in
   *pearl*)
     NUM_GPUS=4
     BATCH_SIZE=512
-    ARM_ONLY=true
-    USE_WRIST=true
+    ARM_ONLY=false
+    USE_WRIST=false
     ACTION_REP=REL
     ulimit -n 65535
     ;;
@@ -39,7 +39,8 @@ case "${SERVER}" in
 esac
 
 BASE_MODEL="nvidia/GR00T-N1.6-3B"
-DATASET_PATH="./data/jkim50104/ffw_sg2_rev1_clear_item"
+DATASET_NAME="ffw_sg2_rev1_base_drive_test"
+DATASET_PATH="./data/jkim50104/$DATASET_NAME" # ffw_sg2_rev1_clear_item, ffw_sg2_rev1_base_drive_test
 EMBODIMENT_TAG="NEW_EMBODIMENT"
 
 CONFIG="ai_worker_config.py"
@@ -68,12 +69,12 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$NUM_GPUS \
   --base-model-path "${BASE_MODEL}" \
   --dataset-path "${DATASET_PATH}" \
   --embodiment-tag "${EMBODIMENT_TAG}" \
-  --modality-config-path "./data/jkim50104/${CONFIG}" \
+  --modality-config-path "./help_scripts/data_config/${CONFIG}" \
   --num-gpus "${NUM_GPUS}" \
-  --output-dir "./output/ai_worker_${HYPER_PARAMS}" \
+  --output-dir "./output/$DATASET_NAME/${HYPER_PARAMS}" \
   --save-total-limit 3 \
   --save-steps 5000 \
-  --max-steps 30000 \
+  --max-steps 10000 \
   --use-wandb \
   --global-batch-size "${BATCH_SIZE}" \
   --color-jitter-params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
